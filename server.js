fba0c78b4f5f45f32592871d2c30c7603aaa90e4
CodeMaster2099 <id2497510@gmail.com>
2026-05-15 14:51:36 +0100

    Fix server.js order

--- server.js
+++ server.js
@@ -1,7 +1,3 @@
-app.get("/", (req, res) => {
-  res.send("Backend is running 🚀");
-});
-
 const express = require("express");
 const { spawn } = require("child_process");
 const multer = require("multer");
@@ -11,6 +7,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Root route
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
 // Upload add-ons
 const upload = multer({ dest: "bedrock/behavior_packs/" });
 
@@ -18,38 +19,40 @@ const upload = multer({ dest: "bedrock/behavior_packs/" });
 let bedrockProcess = null;
 
 app.get("/start", (req, res) => {
-    if (bedrockProcess) return res.send("Server already running");
+  if (bedrockProcess) return res.send("Server already running");
 
-    bedrockProcess = spawn("./bedrock_server", [], {
-        cwd: "./bedrock"
-    });
+  bedrockProcess = spawn("./bedrock_server", [], {
+    cwd: "./bedrock"
+  });
 
-    bedrockProcess.stdout.on("data", data => {
-        console.log(data.toString());
-    });
+  bedrockProcess.stdout.on("data", data => {
+    console.log(data.toString());
+  });
 
-    bedrockProcess.stderr.on("data", data => {
-        console.error(data.toString());
-    });
+  bedrockProcess.stderr.on("data", data => {
+    console.error(data.toString());
+  });
 
-    bedrockProcess.on("close", () => {
-        bedrockProcess = null;
-    });
+  bedrockProcess.on("close", () => {
+    bedrockProcess = null;
+  });
 
-    res.send("Bedrock server started");
+  res.send("Bedrock server started");
 });
 
 // Stop Bedrock server
 app.get("/stop", (req, res) => {
-    if (!bedrockProcess) return res.send("Server not running");
+  if (!bedrockProcess) return res.send("Server not running");
 
-    bedrockProcess.stdin.write("stop\n");
-    res.send("Stopping server");
+  bedrockProcess.stdin.write("stop\n");
+  res.send("Stopping server");
 });
 
 // Upload add-on
 app.post("/upload-addon", upload.single("addon"), (req, res) => {
-    res.send("Add-on uploaded");
+  res.send("Add-on uploaded");
 });
 
-app.listen(3000, () => console.log("Web API running on port 3000"));
+app.listen(3000, () => {
+  console.log("Web API running on port 3000");
+});
